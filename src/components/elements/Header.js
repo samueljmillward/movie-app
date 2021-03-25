20448636e32c79fc28bbe8960d7a195e3d4c7819
samueljmillward <samuel.j.pearce@hotmail.com>
2021-03-25 16:55:25 +0000

    Add Header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
-const Header = () => {
-  return <div>Header</div>;
-};
+import RMDBLogo from '../images/reactMovie_logo.png';
+import TMDBLogo from '../images/tmdb_logo.svg';
+
+import StyledHeader from '../styles/StyledHeader';
+
+const Header = () => (
+  <>
+    <div className="header-content">
+      <img src={RMDBLogo} alt="rmdb-logo" />
+      <img src={TMDBLogo} alt="tmdb-logo" />
+    </div>
+  </>
+);
 export default Header;
