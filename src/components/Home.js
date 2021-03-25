5973293bebebce02855a2a631314e2dde1dfb659
samueljmillward <samuel.j.pearce@hotmail.com>
2021-03-25 16:06:59 +0000

    Add fetchMovies hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,26 @@
 import React from 'react';
 
 import Grid from './elements/Grid';
-import Header from './elements/Header';
 import HeroImage from './elements/HeroImage';
 import LoadMore from './elements/LoadMore';
 import MovieThumb from './elements/MovieThumb';
 import SearchBar from './elements/SearchBar';
 
-const Home = () => (
-  <>
-    <HeroImage />
-    <SearchBar />
-    <Grid />
-    <MovieThumb />
-    <LoadMore />
-  </>
-);
+import { useFetchMovies } from './hooks/useFetchMovies';
+
+const Home = () => {
+  const [{ state, loading, error }, fetchMovies] = useFetchMovies();
+  console.log({ state });
+
+  return (
+    <>
+      <HeroImage />
+      <SearchBar />
+      <Grid />
+      <MovieThumb />
+      <LoadMore />
+    </>
+  );
+};
 
 export default Home;
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+
+export { API_URL, API_KEY };
