aa0e047f89c14e7b4ed9475187df49061b0de6b3
samueljmillward <samuel.j.pearce@hotmail.com>
2021-08-12 23:29:31 +0100

    Add HeroImage styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
     <>
       {state.results[0] ? (
         <HeroImage
-          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}`}
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
           title={state.results[0].original_title}
           text={state.results[0].overview}
         />
