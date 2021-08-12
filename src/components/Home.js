3386089a91c99676c95375d48dba051cfefd49a7
samueljmillward <samuel.j.pearce@hotmail.com>
2021-08-12 23:44:47 +0100

    Add Grid component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 import HeroImage from './HeroImage';
+import Grid from './Grid';
 
 import { useHomeFetch } from '../hooks/useHomeFetch';
 
@@ -20,6 +21,11 @@ const Home = () => {
           text={state.results[0].overview}
         />
       ) : null}
+      <Grid header='Popular Movies'>
+        {state.results.map((movie) => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
     </>
   );
 };
